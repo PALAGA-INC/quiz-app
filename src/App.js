1f6309125e246de75d1d5335f853c76b162ceda2
PALAGA-INC <palagaisiala92@gmail.com>
2021-06-25 11:52:51 +0800

    be great

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Quiz from './components/Quiz';
-import Quiz2 from './components/Quiz2';
+
 
 
 function App() {
   return (
     <div className="app">
-      <Quiz2/>
+      <Quiz/>
     </div>
   )
 }
